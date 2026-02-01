71af4a43e2de7eeeb7868a02ea32eb8117b98343
Василий <greybasil2000@yandex.ru>
2026-02-01 16:55:19 +0300

    IsManager check V1

--- .apex/src/lwc/itemPurchaseTool/itemPurchaseTool.js
+++ .apex/src/lwc/itemPurchaseTool/itemPurchaseTool.js
@@ -1,14 +1,24 @@
 import { LightningElement, api, wire } from 'lwc';
 import { getRecord } from 'lightning/uiRecordApi';
+import updateIsManager from '@salesforce/apex/CheckUserIsManagerController.updateIsManager';
 
 const FIELDS = ['Account.Name', 'Account.AccountNumber', 'Account.Industry'];
 
 export default class ItemPurchaseTool extends LightningElement {
     @api recordId;
     accountData;
+    isManager = false;
 
     @wire(getRecord, { recordId: '$recordId', fields: FIELDS })
     wiredAccount({ error, data }) {
+        updateIsManager()
+            .then(result => {
+                this.isManager = result;
+            })
+            .catch(error => {
+                console.error('Error updating IsManager:', error);
+            });
+
         if (data) {
             this.accountData = {
                 name: data.fields.Name.value,
@@ -22,5 +32,8 @@ export default class ItemPurchaseTool extends LightningElement {
 
     connectedCallback() {
         console.log('recordId:', this.recordId);
+
     }
+
+
 }
